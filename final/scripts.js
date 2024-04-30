4b737da85aef80e77edaab0e345ac437bcc0d7f3
caitlyn-kantaparn <157763542+caitlyn-kantaparn@users.noreply.github.com>
2024-04-30 17:44:02 -0400

    final

---
+++ final/scripts.js
@@ -0,0 +1,20 @@
+
+
+  $('.close-btn').click(function(){
+    $('.popup').removeClass('active');
+  });
+  
+  $('.a').click(function(){
+    $('.popup').removeClass('active');
+    $('.a-p').addClass('active');
+  });
+  
+  $('.b').click(function(){
+    $('.popup').removeClass('active');
+    $('.b-p').addClass('active');
+  });
+  
+  $('.c').click(function(){
+    $('.popup').removeClass('active');
+    $('.c-p').addClass('active');
+  });
